f2de7cd25d4c823b93965f49ae129b45ccb28d64
Sanuka Sithum Gunarathna <106462234+sithumgithub@users.noreply.github.com>
2023-10-18 23:19:26 +0530

    action

--- js/script.js
+++ js/script.js
@@ -23,6 +23,12 @@ window.onscroll = () => {
                     links.classList.remove('active');
                     document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                 });
+                //active sections for animation on scroll
+                sec.classList.add('show-animate');
+            }
+            //if want to use animation that repeats on scroll use this
+            else{
+                sec.classList.remove('show-animate');
             }
         });
 
@@ -31,6 +37,13 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    
+    //remove toggle icon and navbar when click navbar links(scroll)
 
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+    //animation footer an scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
